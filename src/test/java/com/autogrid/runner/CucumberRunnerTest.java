d1d1a8affc54a0eca10b0d62dbc4bec362977aa8
Kavita <138587783+kavitasahu035@users.noreply.github.com>
2024-11-27 16:20:15 +0530

    Revert "Oem nagaraju"

--- src/test/java/com/autogrid/runner/CucumberRunnerTest.java
+++ src/test/java/com/autogrid/runner/CucumberRunnerTest.java
@@ -4,12 +4,10 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-        features = "src/test/resources/features/DMSLogin.feature",  // Relative path from the root directory
+        features = "src/test/resources/features",  // Relative path from the root directory
         glue = {"com.autogrid.stepDefinitions", "com.autogrid.hooks", "com.autogrid.utils"},
         plugin = {"pretty", "html:target/cucumber-reports.html"},
-        monochrome = true,
-        tags="@test"
-
+        monochrome = true
 )
 public class CucumberRunnerTest extends AbstractTestNGCucumberTests {
 
