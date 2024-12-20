bde39df764f067218ef10bed8166edc2e779ce2a
Anjali <anjuuu.0731@gmail.com>
2024-12-20 18:53:51 +0530

    added cucumber folder for reports

--- src/test/java/com/autogrid/runner/CucumberRunnerTest.java
+++ src/test/java/com/autogrid/runner/CucumberRunnerTest.java
@@ -6,9 +6,10 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
         features = "src/test/resources/features",  // Relative path from the root directory
         glue = {"com.autogrid.stepDefinitions", "com.autogrid.hooks", "com.autogrid.utils"},
-        plugin = {"pretty", "html:target/cucumber-reports.html"},
+        plugin = {"pretty", "html:target/cucumber-reports.html","json:target/cucumber-reports/Cucumber.json"},
         monochrome = true,
         tags="@Booking"
+
 
 )
 public class CucumberRunnerTest extends AbstractTestNGCucumberTests {
