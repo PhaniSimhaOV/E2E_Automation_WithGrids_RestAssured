d8fe4d4c1c5685cc3e2820cae16f951f358c38a2
Anjali <anjuuu.0731@gmail.com>
2024-12-20 18:24:35 +0530

    added

--- src/test/java/com/autogrid/stepDefinitions/BookingSalesOperationStepDefinition.java
+++ src/test/java/com/autogrid/stepDefinitions/BookingSalesOperationStepDefinition.java
@@ -88,6 +88,7 @@ public class BookingSalesOperationStepDefinition {
 
     @And("After successful registration user clicks on Quotation")
     public void afterSuccessfulRegistrationUserClicksOnQuotation() throws InterruptedException {
+       Thread.sleep(3000);
         bookingPage.QuotationPage();
 
     }
